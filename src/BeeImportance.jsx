701a03037c49f1e5623b39743c124fda2b4527e2
andre-goncalves-kappa <andrefilipe.gg@gmail.com>
2021-03-13 14:01:25 +0000

    important beeees

--- src/BeeImportance.jsx
+++ src/BeeImportance.jsx
@@ -6,28 +6,49 @@ const BeeImportance = () => {
 
 
     return (
-            
+
         <div className="BeeImportance" >
-            <h1>Why are they so important</h1>
-   <div className="BeeText">
-              <p> A study found that just two percent of wild bee species contributes 80 percent of the crop pollination visits observed globally. This means that if this small percentage of bees disappears then 80 percent of our agricultural system will collapse.
+            <div className="topiccontainer">
+                <h1>Why are they so important</h1>
+                <p> A study found that just two percent of wild bee species contributes 80 percent of the crop pollination visits observed globally. This means that if this small percentage of bees disappears then 80 percent of our agricultural system will collapse.
                 Seventy of the top 100 food crops grown worldwide rely on pollinators, this is equivalent to 90 percent of the world’s nutrition. It might sound unbelievable, but without bees, we can say goodbye to food such as apples, almonds, oranges, and avocados. Considering that around 850 million people across the world are currently suffering from lack of food, coupled with the fact that the global population is set to balloon up to nine billion by 2050, we’re going to need all bees on deck if we hope to avoid mass food scarcity.
                 While this is arguably the most pressing impact of the loss of bees, there are also very real economic consequences that follow.
                 This same study was able to value bees in regards to their value to our economy and researchers concluded that bees contribute around $3,250 to crop production per hectare per year. To put that in context, the UN Food and Agriculture Organization estimates that 1.4 billion hectares of land across the world are currently being cultivated for crops. That equals out to being around $4.2 trillion added to the global economy, all thanks to little bees.
-                </p> 
-                </div>
-                <iframe width="800" height="450" src="https://www.youtube.com/embed/JilYBVrFiLA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                
-                <h1>How Bees Contribute to the Ecosystem</h1>
-                <div className="BeeText">
-                <p>
-               Bees are not only extremely important for humans, but also for entire ecosystems to function. As we know, bees allow plants to reproduce through pollination. These plants contribute to the food system by feeding animals – aside from humans – such as birds and insects. If the food source for these animals was diminished or lost completely, it would cause the entire food chain to suffer.
-               Additionally, about 80 percent of flower plants depend on pollination. If this process stops, not only do we have the potential to lose beautiful plants, but also food for us, birds, squirrels, and all of the other animals that depend on plants for food.
                 </p>
+                <div className="beevideo">
+                    <iframe width="800" height="450" src="https://www.youtube.com/embed/JilYBVrFiLA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
-                
+                <br />
+                <br />
+            </div>
+            <br />
+            <br />
+            <div className="topiccontainer">
+                <h1>How Bees Contribute to the Ecosystem</h1>
+                <p>
+                    Bees are not only extremely important for humans, but also for entire ecosystems to function. As we know, bees allow plants to reproduce through pollination. These plants contribute to the food system by feeding animals – aside from humans – such as birds and insects. If the food source for these animals was diminished or lost completely, it would cause the entire food chain to suffer.
+                    Additionally, about 80 percent of flower plants depend on pollination. If this process stops, not only do we have the potential to lose beautiful plants, but also food for us, birds, squirrels, and all of the other animals that depend on plants for food.
+                </p>
+                <div className="beevideo">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/LiczM-w3V-U" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
-                
+                <br />
+                <br />
+                <div className="beephotos">
+                    <img src="https://res.cloudinary.com/dxzkpsxhf/image/upload/v1615639618/shutterstock_1313526959-2_e3uiyo.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dxzkpsxhf/image/upload/v1615639619/bees_carry_polley_over_their_entire_body_pixabay_uaxnpx.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dxzkpsxhf/image/upload/v1615639620/cc_bees_16x9_rmsooq.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dxzkpsxhf/image/upload/v1615639621/bee-pollen-1024x514_ikpw8m.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dxzkpsxhf/image/upload/v1615639621/December_por6go.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dxzkpsxhf/image/upload/v1615639621/bee_and_pollen_pyo1lo.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dxzkpsxhf/image/upload/v1615639621/bee-1726659_1920-1024x683_vmiza9.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dxzkpsxhf/image/upload/v1615639622/canstockphoto1946447_nvtabk.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dxzkpsxhf/image/upload/v1615639629/Image-Pollination_Bee_Dandelion_Zoom2_wwqtro.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dxzkpsxhf/image/upload/v1615639629/nature-4949437_1920_wltwty.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+
     );
 };
 
