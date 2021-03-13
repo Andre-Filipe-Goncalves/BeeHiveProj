28e84aff454eae2f39d05040cd2d979f936dfe72
RicLaranjo <ricardo.laranjo1711@gmail.com>
2021-03-13 04:05:13 +0000

    create savingbees

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import './App.css';
+import SavingBees from './components/SavingBeesComponents/SavingBees';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SavingBees />
     </div>
   );
 }
